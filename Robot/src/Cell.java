6a3cccf78b24ca55ea7c9bec720104cd3a6da35a
Michael <arteag56@students.rowan.edu>
2014-11-18 14:27:02 -0500

    Update Cell.java
    Added Cell's rewards

--- Robot/src/Cell.java
+++ Robot/src/Cell.java
@@ -6,11 +6,15 @@
 public class Cell {
 
     private int goodness = 0;
+    private int rewardsUp = 0;
+    private int rewardsDown = 0;
+    private int rewardsLeft = 0;
+    private int rewardsRight = 0;
 
     private int armRot;
     private int rakeRot;
 
-    public Cell(int arm, int rake) {
+    public Cell(int arm, int rake) { //grid positions
         this.armRot = arm;
         this.rakeRot = rake;
     }
@@ -30,5 +34,31 @@ public class Cell {
     public int getRakeRot() {
         return this.rakeRot;
     }
+    
+    public int getrewardsUp() {
+    	return rewardsUp;
+    }
+    public int getrewardsDown() {
+    	return rewardsDown;
+    }
+    public int getrewardsLeft() {
+    	return rewardsLeft;
+    }
+    public int getrewardsRight() {
+    	return rewardsRight;
+    }
+    public void setRewardsUp(int newRewards) {
+    	rewardsUp = newRewards;
+    }
+    public void setRewardsDown(int newRewards) {
+    	rewardsDown = newRewards;
+    }
+    public void setRewardsLeft(int newRewards) {
+    	rewardsLeft = newRewards;
+    }
+    public void setRewardsRight(int newRewards) {
+    	rewardsRight = newRewards;
+    }
+    
 
 }
